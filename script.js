efe6c996dbda27695047757b3010ac8859d9aed6
mysterix <oyewoleibkay111@gmail.com>
2022-06-25 11:44:30 +0100

    First javascript task push to github

---
+++ script.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+const userName = {
+    user: "My name is Ibukun",
+    height: "I am 5'10ft tall",
+    country: "I am from Nigeria"
+};
+
+var(userName.user);
+var(userName.height);
+var(userName.country);
+
+document.write(userName.user);
+document.write("<br>");
+document.write(userName.height);
+document.write("<br>");
+document.write(userName.country);
